6eeb6082b75cbbe1f62c7a1e7900259f97d45736
Denis <d.melnikov@growapp.biz>
2023-10-22 21:45:11 +0300

    создана функця для суммирования зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,16 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+for (let key in salaries) {
+  if (isFinite(salaries[key]))
+ result = result+(salaries[key]);
+}
+  return (result)
+}
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
