963c21e7f1c02cd24d455a60138e3a5bfd6a4889
Denis <d.melnikov@growapp.biz>
2023-10-27 22:45:30 +0300

    Создали функцию для приведения текста вида border-left-width в borderLeftWidth

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+
+  let arrSplit = str.split('-');
+  let arrMap = arrSplit.map((item, index) => {
+  if (index == 0) {
+  return item;
+  } else {
+  return item[0].toUpperCase() + item.slice(1);
+  }
+  });
+ let arrJoin = arrMap.join('');
+ return arrJoin;
 }
