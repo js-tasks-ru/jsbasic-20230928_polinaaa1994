ad16e7e33861621933f26b6301ec1679c49efea9
Denis <d.melnikov@growapp.biz>
2023-10-29 14:03:54 +0300

    Создали функцию showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filterAge = users.filter((items) => {
+    return items.age <= age
+  });
+  let mapArr = filterAge.map((items) => {
+    return `${items.name}, ${items.balance}`; 
+  });
+  return mapArr.join("\n")  // ваш код...
 }
